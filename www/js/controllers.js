96aaa86b1b773be0fdfca93b928cacd76f4dc811
PV Campelo Selano <pv_sf@hotmail.com>
2017-04-02 03:45:54 -0300

    trying to make the popup auth

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,11 +10,11 @@ angular.module('starter.controllers', [])
 
 
     $scope.persistSurvey = function() {
-        this.surveyData.push("nome: " + this.firstName);
-        this.surveyData.push("sobrenome: " + this.lastName);
-        this.surveyData.push("email: " + this.email);
-        this.surveyData.push("tel: " + this.phone);
-        this.surveyData.push("comentários: " + this.comment);
+        this.surveyData.push(" nome: " + this.firstName);
+        this.surveyData.push(" sobrenome: " + this.lastName);
+        this.surveyData.push(" email: " + this.email);
+        this.surveyData.push(" tel: " + this.phone);
+        this.surveyData.push(" comentários: " + this.comment);
         console.log($scope.surveyData);
         $window.localStorage.setItem(this.firstName, this.surveyData);
         this.surveyData = [];
@@ -24,16 +24,6 @@ angular.module('starter.controllers', [])
         this.phone = "";
         this.comment = "";
     }
-
-    $scope.localstorage = [];
-
-    for (var i = 0, len = $window.localStorage.length; i < len; i++) {
-        var key = $window.localStorage.key(i);
-        var value = $window.localStorage[key];
-        console.log("Pessoa " + i + " -> " + value);
-        $scope.localstorage.push("Pessoa " + i + " -> " + value);
-    }
-
 })
 
 // .controller('ChatsCtrl', function($scope, Chats) {
@@ -55,8 +45,82 @@ angular.module('starter.controllers', [])
 //     $scope.chat = Chats.get($stateParams.chatId);
 // })
 
-// .controller('AccountCtrl', function($scope) {
-//     $scope.settings = {
-//         enableFriends: true
-//     };
-// });
+.controller('AccountCtrl', function($scope, $window, $state, $ionicPopup, $timeout, $q) {
+
+    // Triggered on a button click, or some other target
+
+    $scope.$on("$ionicView.beforeEnter", function(event, data) {
+        // handle event
+        // $scope.showPopup = function() {
+        $scope.data = {};
+
+        $ionicPopup.prompt({
+            title: 'Password Check',
+            template: 'Enter your secret password',
+            title: 'Enter Password',
+            inputType: 'password',
+            inputPlaceholder: 'Your password',
+            scope: $scope,
+            buttons: [{
+                    text: 'Cancel',
+                    onTap: function(e) {
+                        return false;
+                    }
+                },
+                {
+                    text: '<b>Confirm</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if ($scope.data.wifi === 12345) {
+                            return true;
+                        }
+                    }
+                },
+            ]
+        }).then(function(res) {
+            if (res === true) {
+                $state.go("tab.account");
+            } else {
+                $state.go("tab.dash");
+            }
+            console.log('Tapped!', res);
+        }, function(err) {
+            $state.go("tab.dash");
+            console.log('Err:', err);
+        }, function(msg) {
+            console.log('message:', msg);
+        });
+
+
+        // };
+
+
+        // $ionicPopup.prompt({
+        //     title: 'Password Check',
+        //     template: 'Enter your secret password',
+        // inputType: 'password',
+        // inputPlaceholder: 'Your password'
+        // }).then(
+        //     function(res) {
+        //         console.log(res);
+        //         if (res === 12345) {
+        //             console.log("ok");
+        //             $state.go("tab.account");
+        //         } else {
+        //             $state.go("tab.dash");
+        //         }
+        //     });
+
+    });
+
+
+    $scope.localstorage = [];
+
+    for (var i = 0, len = $window.localStorage.length; i < len; i++) {
+        var key = $window.localStorage.key(i);
+        var value = $window.localStorage[key];
+        console.log("Pessoa " + i + " -> " + value);
+        $scope.localstorage.push("Pessoa " + i + " -> " + value);
+    }
+
+});
